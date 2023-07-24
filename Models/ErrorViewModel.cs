9b3bdd354786d630e8fd61ba6d9d35e26479f7a2
per-sa <ccolombomatheus@gmail.com>
2023-07-24 22:14:09 +0100

    dotnet + tailwind init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace quidvis.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["**/*.razor", "**/*.cshtml", "**/*.html"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
